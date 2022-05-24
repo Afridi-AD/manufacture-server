bde989613051af90421d86ae6e39d26c619fa620
Afridi <thasinafridi8924@gmail.com>
2022-05-24 10:39:23 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wiiut.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+ async function run(){
+     try{
+        await client.connect();
+        const productCollection =client.db('car_tools').collection('products');
+        app.get('/products', async(req,res)=>{
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+
+
+        })
+     }
+  finally{
+
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('car!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
